71cb46095a9930fd8361c2202c90d57c7c845d7b
kairasingh9905 <kairasingh9905@gmail.com>
2026-09-01 09:48:09 +0530

    added titanic database

---
+++ PANDAS/practice.py
@@ -0,0 +1,19 @@
+import pandas as pd
+import numpy as np
+from sklearn.model_selection import train_test_split
+df=pd.read_csv("titanic.csv")
+print(df.shape)
+print(df.head())
+print(df.info())
+
+
+
+
+
+
+
+
+
+
+
+
